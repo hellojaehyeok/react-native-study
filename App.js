9cc0006f890c1e3e91e8a9e2ca87de7de6600d4f
Song jae hyeok <wakij6587@naver.com>
2022-01-30 17:29:05 +0900

    react native init

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
