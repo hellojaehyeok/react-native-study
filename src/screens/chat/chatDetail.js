f02a926a539036a61dc208eda4394b8da7979db5
Song jae hyeok <wakij6587@naver.com>
2022-02-07 10:46:25 +0900

    역무한스크롤 구현 중

--- src/screens/chat/chatDetail.js
+++ src/screens/chat/chatDetail.js
@@ -1,6 +1,6 @@
 import { useFocusEffect } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import ReactNative, { FlatList, Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -20,8 +20,8 @@ const YourChat = () => {
     )
 }
 
-const getChatData = () => {
-    let dataArr = [];
+const getChatData = (arr) => {
+    let dataArr = arr;
     for(let i=0 ; i < 30 ; i++){
         dataArr.push(Math.round(Math.random())===0)
     }
@@ -29,16 +29,65 @@ const getChatData = () => {
 }
 
 const ChatDetail = (props) => {
-    const [chatData, setChatData] = useState(getChatData());
+    const chatScrollRef = useRef();
+    const chatContentRef = useRef();
+    const [chatData, setChatData] = useState([]);
+    const [isFirstRender, setIsFirstRender] = useState(true);
+    const [totalChatHeight, setTotalChatHeight] = useState(0);
+
+
+    useEffect(()=>{
+        let chatArr = getChatData([]);
+        setChatData([...chatArr])
+    }, [])
+
+    const onScrollChat = async e => {
+        // 현재 스크롤 값
+        let updateScroll = e.nativeEvent.contentOffset.y;
+        // 전체 문서의 높이
+        let documentHeight = e.nativeEvent.contentSize.height;
+
+        if(updateScroll == 0){
+            setTotalChatHeight(documentHeight);
+            // 서버 통신
+            let chatArr = getChatData(chatData);
+            setChatData([...chatArr])
+        }
+    }
+    const onChangeChatSize = () => {
+        if(isFirstRender){setIsFirstRender(false); return}
+        chatContentRef.current.measureLayout(ReactNative.findNodeHandle(chatScrollRef.current), (xPos, yPos, Width, Height) => {
+            chatScrollRef.current.scrollTo({x:0, y:Height - totalChatHeight, animated: false});
+        })
+
+    }
+    useEffect(() => {
+        if(chatData.length <= 10){
+            chatScrollRef.current.scrollToEnd({animated: false});
+            setIsFirstRender(false);
+        }
+    }, [chatData])
 
     return(
-        <SafeAreaView style={styles.container}>
-            <FlatList 
-                data={chatData}
-                renderItem={({item}) => item?<MyChat/>:<YourChat />}
-                keyExtractor={(i,index)=>index}
-            />
-        </SafeAreaView>
+        <ScrollView
+            onScroll={onScrollChat}
+            ref={chatScrollRef}
+            onContentSizeChange={onChangeChatSize}
+        >
+            <View ref={chatContentRef}>
+                {
+                    chatData.map((item, index) => {
+                        return (
+                            <View>
+                                <Text>{index}</Text>
+                                {item?<MyChat/>:<YourChat />}
+                            </View>
+                        )
+                        ;
+                    })
+                }
+            </View>
+        </ScrollView>
     )
 };
 
